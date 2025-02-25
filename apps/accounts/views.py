5ffeb2b1be64e60996a138495bd8a3e504c20553
Fatima <melodygeorge6547@gmail.com>
2025-02-25 17:08:40 +0500

    adding auth templates

---
+++ apps/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def user_register(request):
+    return render(request, 'accounts/register.html')
+
+
+def user_login(request):
+    return render(request, 'accounts/login.html')

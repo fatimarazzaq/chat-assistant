33d3d8f91a11cae57608c80cb8286365cae2a47a
Fatima <melodygeorge6547@gmail.com>
2025-02-26 20:53:38 +0500

    added chat page

--- apps/chatbot/views.py
+++ apps/chatbot/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from openai import OpenAI
 
 # Create your views here.
+
+api_key="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"
+
+client = OpenAI(api_key=api_key)
+
+def chat_response(message):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    return completion.choices[0].message.content
+
+def chat(request):
+    if request.method == "POST":
+        user_message = request.POST.get("message", "")
+        bot_reply = chat_response(user_message)
+        return JsonResponse({"response": bot_reply})
+
+    return render(request, "chatbot/chat.html")
--- dj_chatbot/urls.py
+++ dj_chatbot/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.chatbot.urls')),
     path('accounts/', include('apps.accounts.urls')),
 ]
 

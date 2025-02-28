db445ce5951d7e086e1fe2c0fbac32ea351d0bfb
Fatima <melodygeorge6547@gmail.com>
2025-02-28 14:08:18 +0500

    added authentication system

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('register/', user_register, name='register'),
     path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 
 def user_register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match!")
+            return redirect("register")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already taken!")
+            return redirect("register")
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email is already in use!")
+            return redirect("register")
+
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        user.save()
+        messages.success(request, "Your account has been created successfully! You can now log in.")
+        return redirect("login")
+
+    return render(request, "accounts/register.html")
+
 
 
 def user_login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in!")
+            return redirect("chat_home")  # Change "home" to the name of your homepage URL pattern
+        else:
+            messages.error(request, "Invalid username or password!")
+
+    return render(request, "accounts/login.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
